f7643bde444ec594cae4b68800d67e3d8ed807df
Raitis ZE <raitis.zgirskis@gmail.com>
2024-04-08 17:58:59 +0300

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Raitis ZE on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
